7e6d035ffcd8b27905f57005209a2bb71829e4a8
renzo marcos de la torre <renzochepu12345@gmail.com>
2019-11-24 19:03:57 -0500

    Segundo commit

---
+++ src/Index.js
@@ -0,0 +1,34 @@
+const express = require('express'); // Requiere el módulo Express y se almacena en una constante del mismo nombre
+const path = require('path');       // Requiere el módulo path que viene junto con Node
+const exphbs = require('express-handlebars')
+
+// Inicializaciones 
+const app = express();
+
+
+// Configuraciones (Settings)
+app.set('port', process.env.PORT || 3000); // Crea configuración en el puerto 3000 si no existe otro en nuestra ordenador
+app.set('views', path.join(     dirname, 'views')); //Sirve para indicarle a Node que la carpeta se encuentra dentro de Src, dirname (constante de Node)
+app.engine('.hbs', exphbs({
+    defaultLayout:,
+    layoutsDir:,
+    partialsDir:,
+    extname:
+})); 
+
+// Middlewares (Funciones que serán ejecutadas)
+
+
+
+// Global Variables
+
+
+
+//Static Files
+
+
+
+//Server is listenning
+app.listen(app.get('port'), () => {
+    console.log('Servidor en el puerto', app.get('port')); // Si está en fucionamiento que me arroje este mensaje () => es lo mismo que colocar Function()
+});
