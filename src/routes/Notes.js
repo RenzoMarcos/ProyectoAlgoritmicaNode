91f4270dd910f891010b1e1468ec5f16e2ac80ae
renzo marcos de la torre <renzochepu12345@gmail.com>
2019-11-28 13:03:53 -0500

    Front-end y registro

--- src/routes/Notes.js
+++ src/routes/Notes.js
@@ -1,7 +1,17 @@
 const router = require('express').Router();
 
+router.get('/notes/add', (request, response) =>{
+    response.render('notes/new-note')
+});
+
+router.post('/notes/new-note', (request, response) => {
+    console.log(request.body);
+    response.send('ok');
+});
+
+
 router.get('/notes', (request, response) =>{
     response.send('Notas desde la base de datos')
-})
+});
 
 module.exports = router;
