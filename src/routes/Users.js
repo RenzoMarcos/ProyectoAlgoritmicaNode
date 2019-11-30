3de8630e30915aa9089f0e89d347aa70a01bc978
renzo marcos de la torre <renzochepu12345@gmail.com>
2019-11-29 21:52:26 -0500

    Carrito

--- src/routes/Users.js
+++ src/routes/Users.js
@@ -8,4 +8,23 @@ router.get('/Users/signup', (request, response) =>{
     response.render('Users/Signup');
 });
 
+router.post('/Users/signup', (request, response) =>{
+    const { name, email, password, confirm_password} = request.body;
+    const errors = [];
+    if(password != confirm_password){
+        errors.push({text: 'Las contraseñas no coinciden'});
+    }
+    if(password.lenght < 4){
+        errors.push({text: 'La contraseña debe tner por lo menos 4 dígitos'});
+    }
+    if(errors.lenght > 0){
+        response.render('Users/signup', {errors, name, email, password, confirm_password});
+    } 
+    else{
+            response.send('Gaaaaaaaa');
+        }
+    
+    response.send('Gaaaaa');
+});
+
 module.exports = router;
